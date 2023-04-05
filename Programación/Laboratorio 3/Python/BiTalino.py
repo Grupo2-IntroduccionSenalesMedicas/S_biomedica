731dfa038c0646785dd4a0f966ae2b9bd25cece8
lbarretoe <luis.barreto@upch.pe>
2023-04-05 12:25:04 -0500

    Lab 3

--- Programación/Laboratorio 3/Python/BiTalino.py
+++ Programación/Laboratorio 3/Python/BiTalino.py
@@ -1,1 +1,10 @@
-print('Hello World')
+import numpy as np
+import matplotlib.pyplot as plt
+
+f = np.loadtxt('File.txt')
+
+plt.figure(1)
+plt.plot(f,'r')
+plt.title('Obtained Signal')
+plt.xlabel('Time');plt.ylabel('Amplitude')
+plt.grid(True);plt.show()
